21fe005f3d31a608df719417929a791e9ef83ab8
Artyom Katsap <artyom.katsap@gmail.com>
2011-11-26 17:02:24 +0200

    added some helper functions to categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,18 @@
 class Category < ActiveRecord::Base
   acts_as_tree :order=>"name"
   has_and_belongs_to_many :products
+  
+  #helper method to extract all top level categories
+  def self.top_categories
+    select("*").where("parent_id is null")
+  end
+  
+  #helper method to extract all sub categories
+  def self.sub_categories
+    select("*").where("parent_id is not null")
+  end
+  
+  def top_level?
+    parent_id.nil?
+  end
 end
