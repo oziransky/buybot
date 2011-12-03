99aadd37f33fffdfcd27dde879f59f4cd800a171
Artyom Katsap <artyom.katsap@gmail.com>
2011-12-03 12:43:45 +0200

    added search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :url, :catalog_id, :image_folder, :manufacturer, :store_id
   belongs_to :store
@@ -5,4 +6,19 @@ class Product < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :manufacturer,  :presence => true
+  
+  
+  
+  def self.search(search)
+    if search
+      where("name LIKE ?","%#{search}%")
+      #find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
+  def minimum_price
+    100+id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Buybot::Application.routes.draw do
 
+
+  get "customer_products/search"
+
   devise_for :store_owners
   devise_for :users
 
@@ -11,5 +14,5 @@ Buybot::Application.routes.draw do
   match '/help', :to => 'pages#help'
   match '/invite_friends', :to => 'pages#invite_friends'
 
-  root :to => 'pages#home'
+  root :to => 'categories#index'
 end
