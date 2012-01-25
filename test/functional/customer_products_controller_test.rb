713917e4c4afff3584f578b01a5a6bfb8b1254b2
Michael Oziransky <oziransky@gmail.com>
2012-01-25 11:44:15 +0200

    Add specs for auction controller Remove the tests approach

--- test/functional/customer_products_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CustomerProductsControllerTest < ActionController::TestCase
-  test "should get search" do
-    get :search
-    assert_response :success
-  end
-
-end
--- test/functional/stores_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StoresControllerTest < ActionController::TestCase
-  test "should get stores" do
-    get :stores
-    assert_response :success
-  end
-
-end
