47e821ad779e88ed1b2bf327751bcc3d6c7e2531
Artyom Katsap <artyom.katsap@gmail.com>
2012-02-07 22:31:43 +0200

    added more tests for product model

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -1,22 +1,37 @@
 require 'spec_helper'
 
 describe 'Product model' do
-    
+	
     fixtures :categories, :products
     
     describe 'search' do
         #this test is strongly connected to the fixtures.
         #changing the fixtures may break this tests.
-       it "should find by prodct by name" do
+		it "should find by a product by name" do
            
            category = categories(:electronics)
-           prod = Product.search :search=>"Tele", 
+           prod = Product.search :search=>"Tele"
+           
+           prod.size.should eq(2)
+           assert prod[1].name.start_with?("Television")
+           assert prod[0].name.start_with?("Television")
+		end
+		it "should filter the search results by category and manufacturer" do
+           
+			category = categories(:electronics)
+			prod = Product.search :search=>"Tele", 
                                  :categories=>category.id, 
                                  :manufacturer=>"Toshiba"
            
-           prod.should_not be_empty
-           prod[0].name  == "Television2"
-       end 
+			prod.should_not be_empty
+			prod[0].name  == "Television2"
+		end 
+		it "product is not valid without a name" do
+			prod = Product.new(:name=>"",:manufacturer=>"Sony").should_not be_valid
+		end
+		it "product is not valid without a manufacturer" do
+			prod = Product.new(:name=>"some product",:manufacturer=>"").should_not be_valid
+		end
         
     end
     
