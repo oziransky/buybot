300b735e83fa6864bf5330fe1bfc3eb2cec15aff
Michael Oziransky <oziransky@gmail.com>
2012-03-03 21:10:00 +0200

    Add view of the rating to the store auction views. Fix some view errors.

--- app/helpers/store_auctions_helper.rb
+++ app/helpers/store_auctions_helper.rb
@@ -10,4 +10,14 @@ module StoreAuctionsHelper
     }
     list.sort!
   end
+
+  def user_rate(auction)
+    store_id = session[:current_store_id]
+    rate = auction.auction_statuses.where("store_id = ?", store_id).first.user_rate
+    if not rate.nil?
+      rate
+    else
+      "N/A"
+end
+  end
 end
